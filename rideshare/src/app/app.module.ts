f2e1d875d39127b533f90db5d5849c22355017b1
niharika saitana <niharikasaitana@email.com>
2024-07-22 22:06:03 +0530

    done search layout

--- rideshare/src/app/app.module.ts
+++ rideshare/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+    providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rideshare/src/app/home/home.component.ts
+++ rideshare/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  homeForm:FormGroup;
+  minDate = new Date();
+  AdultCount=1;
+  constructor(private fb: FormBuilder,private datePipe: DatePipe) {
+    this.homeForm = this.fb.group({
+      Source: ['', Validators.required],
+      Destination: ['', Validators.required],
+      startdate: [new DatePipe('en-US').transform(this.minDate, 'yyyy-MM-dd')],
+    
+      Adultcount: [1,Validators.required],
+    });
+   }
 
   ngOnInit(): void {
+   
   }
 
 }
