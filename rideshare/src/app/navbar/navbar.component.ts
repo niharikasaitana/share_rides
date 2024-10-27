86f201a917a8a81624e3b3f8e4be054c205b58e9
niharikasaitana <niharikasaitana@gmail.com>
2024-10-27 18:30:54 +0530

    sidebar created

--- rideshare/src/app/navbar/navbar.component.ts
+++ rideshare/src/app/navbar/navbar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  sidebarShow: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+   //sidebar open
+   opensettings() {
+    this.sidebarShow = !this.sidebarShow;
+   // this.getprofiledetails();
+  }
+
+  myride() {
+  //  this.router.navigate(['rides']);
+    this.sidebarShow = !this.sidebarShow;
   }
 
 }
